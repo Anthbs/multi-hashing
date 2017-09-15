ebacf9ccaea1a8e383e777e252b4a27f09e096df
Anthbs <Anthbs@gmail.com>
2017-09-15 15:45:55 +1200

    Initial Commit

---
+++ build/Release/.deps/Release/obj.target/multihashing/multihashing.o.d
@@ -0,0 +1,50 @@
+cmd_Release/obj.target/multihashing/multihashing.o := g++ '-DNODE_GYP_MODULE_NAME=multihashing' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/gringotts/.node-gyp/0.10.48/include/node -I/home/gringotts/.node-gyp/0.10.48/src -I/home/gringotts/.node-gyp/0.10.48/deps/uv/include -I/home/gringotts/.node-gyp/0.10.48/deps/v8/include -I../crypto -I../sha3  -fPIC -Wall -Wextra -Wno-unused-parameter -pthread -m64 -O2 -fno-strict-aliasing -fno-tree-vrp -fno-tree-sink -fno-omit-frame-pointer -fno-delete-null-pointer-checks -fno-exceptions -fno-rtti -std=c++0x -MMD -MF ./Release/.deps/Release/obj.target/multihashing/multihashing.o.d.raw   -c -o Release/obj.target/multihashing/multihashing.o ../multihashing.cc
+Release/obj.target/multihashing/multihashing.o: ../multihashing.cc \
+ /home/gringotts/.node-gyp/0.10.48/include/node/node.h \
+ /home/gringotts/.node-gyp/0.10.48/include/node/uv.h \
+ /home/gringotts/.node-gyp/0.10.48/include/node/uv-private/uv-unix.h \
+ /home/gringotts/.node-gyp/0.10.48/include/node/uv-private/ngx-queue.h \
+ /home/gringotts/.node-gyp/0.10.48/include/node/uv-private/uv-linux.h \
+ /home/gringotts/.node-gyp/0.10.48/include/node/v8.h \
+ /home/gringotts/.node-gyp/0.10.48/include/node/v8stdint.h \
+ /home/gringotts/.node-gyp/0.10.48/include/node/node_object_wrap.h \
+ /home/gringotts/.node-gyp/0.10.48/include/node/node.h \
+ /home/gringotts/.node-gyp/0.10.48/include/node/node_buffer.h \
+ /home/gringotts/.node-gyp/0.10.48/include/node/v8.h ../bcrypt.h \
+ ../keccak.h ../quark.h ../blake2s.h ../scryptjane.h ../scryptn.h \
+ ../skein.h ../x11.h ../groestl.h ../blake.h ../fugue.h ../qubit.h \
+ ../hefty1.h ../shavite3.h ../cryptonight.h ../x13.h ../nist5.h ../sha1.h \
+ ../x15.h ../fresh.h ../boolberry.h
+../multihashing.cc:
+/home/gringotts/.node-gyp/0.10.48/include/node/node.h:
+/home/gringotts/.node-gyp/0.10.48/include/node/uv.h:
+/home/gringotts/.node-gyp/0.10.48/include/node/uv-private/uv-unix.h:
+/home/gringotts/.node-gyp/0.10.48/include/node/uv-private/ngx-queue.h:
+/home/gringotts/.node-gyp/0.10.48/include/node/uv-private/uv-linux.h:
+/home/gringotts/.node-gyp/0.10.48/include/node/v8.h:
+/home/gringotts/.node-gyp/0.10.48/include/node/v8stdint.h:
+/home/gringotts/.node-gyp/0.10.48/include/node/node_object_wrap.h:
+/home/gringotts/.node-gyp/0.10.48/include/node/node.h:
+/home/gringotts/.node-gyp/0.10.48/include/node/node_buffer.h:
+/home/gringotts/.node-gyp/0.10.48/include/node/v8.h:
+../bcrypt.h:
+../keccak.h:
+../quark.h:
+../blake2s.h:
+../scryptjane.h:
+../scryptn.h:
+../skein.h:
+../x11.h:
+../groestl.h:
+../blake.h:
+../fugue.h:
+../qubit.h:
+../hefty1.h:
+../shavite3.h:
+../cryptonight.h:
+../x13.h:
+../nist5.h:
+../sha1.h:
+../x15.h:
+../fresh.h:
+../boolberry.h:
